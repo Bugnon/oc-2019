cb91cc5ac68390283b362bac355a5fd3043e9773
Kapestian <val.porchet@gmail.com>
2019-10-03 14:08:32 +0200

    Update ex1_val.py

---
+++ homework/ex1/ex1_val.py
@@ -0,0 +1,9 @@
+bouteille = {
+    "nom" : "Chateau Bibine",
+    "ao" : "Cote de la Saone",
+    "volume" : "75cl",
+    "année de millésime" : "2012"}
+
+for elt in bouteille:
+    i=1
+    print(elt, ":", bouteille[elt])
