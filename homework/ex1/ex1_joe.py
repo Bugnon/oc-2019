05a6f07a40daed22a1bb759d362576ddfda0a3b9
Joël Mosimann <l173297@m218-11.local>
2019-10-03 13:58:49 +0200

    Update ex1_joe.py

--- homework/ex1/ex1_joe.py
+++ homework/ex1/ex1_joe.py
@@ -1,1 +1,6 @@
-# exercice 1 chapitre 8, ex2, 
+# exercice 1 chapitre 8, ex2,
+
+vin = {'domaine':'château Bibine', 'appelation':'côtes de la Saone', 'nbr de centilitre':75, 'année de milésime' :2012}
+
+for a in vin:
+    print(a , ':', vin[a])
