24dd8946f41dc0c8575e525e679b031064678cc7
Maxime Schaller <l162778@m218-15.local>
2020-01-16 15:16:54 +0100

    Update space_game.py

--- games/sch/space_game.py
+++ games/sch/space_game.py
@@ -48,11 +48,6 @@ class Player:
     def draw(self): #draws the player
         App.screen.blit(self.img, self.rect)
         
-    def collide(self):
-        for enemy in self.enemies:
-            if self.rect.centery-Enemy.y<52 and self.rect.centerx-Enemy.x<15:
-                Game.running = False
-    #defines collisions between player and enemy
     
 class Enemy:
     """The player must avoid the enemies."""
@@ -103,6 +98,12 @@ class App:
         #creates background, player and 3 ennemies
         
         self.running = True
+
+    def collide(self):
+        for enemy in self.enemies:
+            if self.rect.centery-Enemy.y<52 and self.rect.centerx-Enemy.x<15:
+                Game.running = False
+    #defines collisions between player and enemy
         
     def run(self):
         print('run app')
